e05784e6f4e937fb1a40a6e85ddce33e974a2ea2
ketanryan <kyurembeats2@gmail.com>
2020-04-07 23:01:29 -0400

    Cool vector math is cool

--- src/main/java/com/mco/herobrinemod/entities/herobrine/phase3/EntityHardestHerobrine.java
+++ src/main/java/com/mco/herobrinemod/entities/herobrine/phase3/EntityHardestHerobrine.java
@@ -21,7 +21,6 @@ public class EntityHardestHerobrine extends EntityMob
         super.onLivingUpdate();
 
         rotationPitch = 37;
-
         laser();
     }
 
@@ -30,32 +29,44 @@ public class EntityHardestHerobrine extends EntityMob
         Vec3d initialVec = this.getPositionEyes(1);
         RayTraceResult rayTrace = this.rayTrace(200,1);
         Vec3d lookFar = rayTrace.hitVec;
-        BlockPos secondPos = new BlockPos(lookFar);
-
-        if (world.getBlockState(secondPos).getMaterial() == Material.AIR &&
-                Blocks.FIRE.canPlaceBlockAt(world, secondPos) && !world.isRemote)
-            world.setBlockState(secondPos, Blocks.FIRE.getDefaultState());
-
-        double diffX = secondPos.getX() - initialVec.x;
-        double diffY = secondPos.getY() - initialVec.y;
-        double diffZ = secondPos.getZ() - initialVec.z;
-
-        double length = Math.sqrt(Math.pow(diffX, 2) + Math.pow(diffY, 2) + Math.pow(diffZ, 2));
-        for (int i = 0; i < length; i++)
+        if(lookFar != null)
         {
-            double factorX = diffX * (i/32.0);
-            double factorY = diffY * (i/32.0);
-            double factorZ = diffZ * (i/32.0);
+            BlockPos secondPos = new BlockPos(lookFar);
 
-            Vec3d factorVec = new Vec3d(factorX, factorY, factorZ);
-            Vec3d slopeVec = initialVec.add(factorVec);
+            setFire(secondPos);
+            setFire(secondPos.east());
+            setFire(secondPos.west());
+            setFire(secondPos.north());
+            setFire(secondPos.south());
 
-            BlockPos slopePos = new BlockPos(slopeVec);
-            if(ticksExisted % 10 == 0)
-                world.spawnParticle(EnumParticleTypes.EXPLOSION_LARGE, slopePos.getX(), slopePos.getY(), slopePos.getZ(),
-                    0,0,0);
+            double diffX = secondPos.getX() - initialVec.x;
+            double diffY = secondPos.getY() - initialVec.y;
+            double diffZ = secondPos.getZ() - initialVec.z;
 
+            double length = Math.sqrt(Math.pow(diffX, 2) + Math.pow(diffY, 2) + Math.pow(diffZ, 2));
+            for (int i = 0; i < length; i++) {
+                double factorX = diffX * (i / 32.0);
+                double factorY = diffY * (i / 32.0);
+                double factorZ = diffZ * (i / 32.0);
+
+                Vec3d factorVec = new Vec3d(factorX, factorY, factorZ);
+                Vec3d slopeVec = initialVec.add(factorVec);
+
+                BlockPos slopePos = new BlockPos(slopeVec);
+                if (ticksExisted % 10 == 0) {
+                    world.setBlockToAir(slopePos);
+                    world.spawnParticle(EnumParticleTypes.EXPLOSION_LARGE, slopePos.getX(), slopePos.getY(), slopePos.getZ(),
+                            0, 0, 0);
+                }
+            }
         }
+    }
+
+    private void setFire(BlockPos pos)
+    {
+        if(world.getBlockState(pos).getMaterial() == Material.AIR && Blocks.FIRE.canPlaceBlockAt(world, pos)
+                && !world.isRemote)
+            world.setBlockState(pos, Blocks.FIRE.getDefaultState());
     }
 
     @Override
