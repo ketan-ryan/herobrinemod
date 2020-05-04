c3c3738d12f3170a96b200bbe04e91a6ba0aa3c6
ketanryan <kyurembeats2@gmail.com>
2020-05-04 19:18:05 -0400

    Laser rotates w mouse

--- src/main/java/com/mco/herobrinemod/entities/herobrine/phase3/RenderHardestHerobrine.java
+++ src/main/java/com/mco/herobrinemod/entities/herobrine/phase3/RenderHardestHerobrine.java
@@ -48,59 +48,62 @@ public class RenderHardestHerobrine extends RenderLiving<EntityHardestHerobrine>
      */
     public void doRender(EntityHardestHerobrine herobrine, double x, double y, double z, float entityYaw, float partialTicks)
     {
-        super.doRender(herobrine, x, y, z, entityYaw, partialTicks);
-
-        GlStateManager.alphaFunc(516, 0.1F);
-        this.bindTexture(BEAM_TEXTURES);
         if(herobrine.getStartPos() != null && herobrine.getEndPos() != null)
         {
             double height = herobrine.getStartPos().y - 10;
-            double totalWorldTime = herobrine.world.getWorldTime();
-            double texScale = 10.0D;
-            double r = 10.5D;
+            GlStateManager.pushMatrix();
+            GlStateManager.translate(x, y + 1.1, z);
+            setupGl();
+            this.bindTexture(BEAM_TEXTURES);
+            GlStateManager.rotate(20, 0, 1, 0);
+            GlStateManager.disableDepth();
             renderLaser(x - 3.8, y + 9, z, partialTicks, herobrine.posX, herobrine.posY, herobrine.posZ,
                     herobrine.ticksExisted, herobrine.getEndPos().x, herobrine.getEndPos().y, herobrine.getEndPos().z,
-                    height, texScale);
-
+                    height, herobrine.rotationYaw, herobrine.rotationPitch);
             renderLaser(x + 3.8, y + 9, z, partialTicks, herobrine.posX, herobrine.posY, herobrine.posZ,
                     herobrine.ticksExisted, herobrine.getEndPos().x, herobrine.getEndPos().y, herobrine.getEndPos().z,
-                    height, texScale);
+                    height, herobrine.rotationYaw, herobrine.rotationPitch);
+            GlStateManager.enableDepth();
+            revertGl();
+            GlStateManager.popMatrix();
         }
+        super.doRender(herobrine, x, y, z, entityYaw, partialTicks);
     }
 
+    private void setupGl()
+    {
+        GlStateManager.blendFunc(GlStateManager.SourceFactor.SRC_ALPHA, GlStateManager.DestFactor.ONE_MINUS_SRC_ALPHA);
+        GlStateManager.enableBlend();
+        GlStateManager.disableLighting();
+        GlStateManager.alphaFunc(GL11.GL_GREATER, 0);
+        GlStateManager.disableCull();
+    }
+
+    private void revertGl()
+    {
+        GlStateManager.disableBlend();
+        GlStateManager.enableLighting();
+        GlStateManager.alphaFunc(GL11.GL_GREATER, 0.1F);
+        GlStateManager.enableCull();
+    }
     private void renderLaser(double x, double y, double z, float partialTicks, double newX, double newY, double newZ,
-                             int ticksExisted, double blockX, double blockY, double blockZ, double height, double textureScale) {
+                             int ticksExisted, double blockX, double blockY, double blockZ, double height, float yaw, float pitch) {
 
         float f = (float)(blockX - newX);
         float f1 = (float)(blockY - 1.0D - newY);
         float f2 = (float)(blockZ - newZ);
         float length = MathHelper.sqrt(f * f + f1 * f1 + f2 * f2) + 5;
 
-        GlStateManager.pushMatrix();
-        GlStateManager.glTexParameteri(3553, 10242, 10497);
-        GlStateManager.glTexParameteri(3553, 10243, 10497);
-        GlStateManager.enableLighting();
-        GlStateManager.disableCull();
-        GlStateManager.disableBlend();
-        GlStateManager.depthMask(true);
-        GlStateManager.tryBlendFuncSeparate(GlStateManager.SourceFactor.SRC_ALPHA, GlStateManager.DestFactor.ONE, GlStateManager.SourceFactor.ONE, GlStateManager.DestFactor.ZERO);
-
         Tessellator tessellator = Tessellator.getInstance();
         BufferBuilder buf = tessellator.getBuffer();
         buf.begin(GL11.GL_QUADS, DefaultVertexFormats.POSITION_TEX_LMAP_COLOR);
-
         buf.pos(-RADIUS + x, height + y, + z).tex(0, 0).lightmap(0,240).color(255, 255, 255, 255).endVertex();
         buf.pos(-RADIUS + x, height+y, length + z).tex(0, 1).lightmap(0,240).color(255, 255, 255, 255).endVertex();
         buf.pos(RADIUS + x, height+y, length + z).tex(1, 0).lightmap(0, 240).color(255, 255, 255, 255).endVertex();
         buf.pos(RADIUS + x, height + y,  z).tex(1, 1).lightmap(0, 240).color(255, 255, 255, 255).endVertex();
-
+        GlStateManager.disableDepth();
         tessellator.draw();
-
-        GlStateManager.enableBlend();
-        GlStateManager.tryBlendFuncSeparate(GlStateManager.SourceFactor.SRC_ALPHA, GlStateManager.DestFactor.ONE_MINUS_SRC_ALPHA, GlStateManager.SourceFactor.ONE, GlStateManager.DestFactor.ZERO);
-        GlStateManager.depthMask(false);
-        GlStateManager.disableBlend();
-        GlStateManager.popMatrix();
+        GlStateManager.enableDepth();
     }
 
     public static class Factory implements IRenderFactory<EntityHardestHerobrine>
